1da31b3a187e4d4f5a7962f5f05ff795cf4d4381
Vijay Vammi <mesanthu@gmail.com>
2016-12-10 18:03:48 +0000

    added a new route

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/helloagain')
+def hello_again():
+	return 'hello again'
 
 if __name__ == '__main__':
 	app.run()
