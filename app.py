2816945cedb986174e903fc5208b9e29ae27fbab
Vijay Vammi <mesanthu@gmail.com>
2016-12-10 18:22:18 +0000

    Dockerizing

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask
+From flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
-@app.route('/helloagain')
-def hello_again():
-	return 'hello again'
+    return 'Flask Dockerized'
 
 if __name__ == '__main__':
-	app.run()
-
+    app.run(debug=True,host='0.0.0.0')
